ce8043d8781a4dbb2024281a850a421326c1d9c5
Apurv099 <apurvgurav.619@gmail.com>
2025-06-23 23:36:00 +0530

    feature: add support for custom delimiters

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -3,11 +3,16 @@ class StringCalculator {
     if (numbers.isEmpty) {
       return 0;
     }
-    // Replace new lines with commas
-    numbers = numbers.replaceAll('\n', ',');
 
-    // Split by comma
-    final tokens = numbers.split(',');
+    String delimiters = ',|\n';
+    if (numbers.startsWith('//')) {
+      final parts = numbers.split('\n');
+      final customDelimiter = RegExp.escape(parts[0].substring(2));
+      delimiters = customDelimiter;
+      numbers = parts.sublist(1).join('\n');
+    }
+
+    final tokens = numbers.split(RegExp(delimiters));
     return tokens.map(int.parse).fold(0, (sum, number) => sum + number);
   }
 }
--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -7,19 +7,23 @@ void main() {
   test('returns 0 for an empty string', () {
     expect(calculator.add(''), equals(0));
   });
-
+  
   test('returns the number itself when one number is present', () {
     expect(calculator.add('5'), equals(5));
   });
-
+  
   test('returns the sum of two comma separated numbers', () {
     expect(calculator.add('1,5'), equals(6));
   });
-
+  
   test('returns the sum of any number of comma separated numbers', () {
     expect(calculator.add('1,2,3,4'), equals(10));
   });
+  
   test('supports new lines as a delimiter', () {
     expect(calculator.add('1\n2,3'), equals(6));
+  });
+  test('supports custom delimiters', () {
+    expect(calculator.add('//;\n1;2'), equals(3));
   });
 }
